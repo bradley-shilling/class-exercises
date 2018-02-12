9d7c48ef022e29e97abaa93ad005aa96e345ed7e
Bradley Shilling <bradley.shilling@gmail.com>
2018-02-12 11:58:28 -0500

    commit to allow pull

--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
@@ -1,10 +1,81 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FileSplitter {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FileNotFoundException {
+		// vars
+		int fileNumber = 0;
 
+		// set up
+		Scanner input = new Scanner(System.in);
+		// prompt user for search term
+		File inputFile = getInputFileFromUser();
+		// prompt user for number of lines to split at
+		System.out.print("Please enter the number of lines each file should contain: ");
+		String userNumber = input.nextLine();
+		int numberOfLines = Integer.parseInt(userNumber); // change to int
+
+		
+		/*
+		 * Logic
+		 */
+		if (numberOfLines >= 1) { // do if number of lines per file is valid
+			int fileCount = 1;
+			// write to file
+			try (Scanner fileReader = new Scanner(inputFile); // read in source
+			) {
+				// loop write to new files
+				while (fileReader.hasNextLine()) {
+					PrintWriter writer = new PrintWriter(inputFile + "-" + fileCount + ".txt");
+					for (int i = 0; i < numberOfLines; i++) { // count to specified number of lines
+						if (fileReader.hasNextLine()) { // only execute while next line exists in input file this prevents end
+							String currentLine = fileReader.nextLine(); // get line from file
+							writer.println(currentLine); // add to new file
+						}
+					}
+					fileCount++; // increase file count
+					writer.close(); // close current file and write to hd !
+				}
+
+			} catch (FileNotFoundException ex) {
+				System.out.println("File not found!"); // tells user the file is unable to be located
+				System.exit(1); // Ends the program
+			}
+
+		} else {
+			System.out.println("Enter a positive integer"); // tells user to enter valid number
+			System.exit(1); // Ends the program
+		}
+
+		// tell user task completed
+		System.out.println("Done!");
+	}
+
+	
+
+	// get input file path method
+	@SuppressWarnings("resource")
+	private static File getInputFileFromUser() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter the path to the original file: ");
+		String path = userInput.nextLine();
+		// path
+		// String path = "alices_adventures_in_wonderland.txt";
+
+		File inputFile = new File(path);
+		if (inputFile.exists() == false) { // checks for the existence of a file
+			System.out.println(path + " does not exist");
+			System.exit(1); // Ends the program
+		} else if (inputFile.isFile() == false) {
+			System.out.println(path + " is not a file");
+			System.exit(1); // Ends the program
+		}
+		return inputFile;
 	}
 
 }
--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
@@ -1,10 +1,44 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		// create new file for output
+		File newFile = new File("FizzBuzz.txt");
 		
-
+		
+		// write to file
+		
+			try(PrintWriter writer = new PrintWriter(newFile)) {
+				for(int i=0; i<301; i++){ // count to 300
+				writer.println(fizzBuzz(i));
+				}
+			}
+		
+		// tell user task completed
+		System.out.println("Done!");
 	}
+	
+	//fizz buzz method
+	public static String fizzBuzz(int input) {
+		if (input == 0) { // check if 0
+			return "0";
+		} else if (input % 15 == 0 || String.valueOf(input).contains("3") && String.valueOf(input).contains("5")) { // check if contains 3 and 5 or is divisable by 3 and 5
+			return "FizzBuzz";
+		} else if (input % 3 == 0 || String.valueOf(input).contains("3")) {  // check if divisable by 3 or contains 3
+			return "Fizz";
+		} else if (input % 5 == 0 || String.valueOf(input).contains("5")) { // check if divisable by 5 or contains 5
+			return "Buzz";
+		} else if (input > 0 && input <301) { // check if in range
+			return String.valueOf(input);
+		}
+			return "Out of range."; // else return out of range
+			
+		}
+
 
 }
